6c7627e1919e99e3f060a7a6fbfd21f521e68711
dtaylor1701 <dktaylor1701@gmail.com>
2018-02-15 20:54:05 -0500

    Initial Commit

---
+++ XKCD Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XKCD Reader
+//
+//  Created by David Taylor on 2/15/18.
+//  Copyright © 2018 Hyper Elephant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ XKCD ReaderTests/XKCD_ReaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  XKCD_ReaderTests.swift
+//  XKCD ReaderTests
+//
+//  Created by David Taylor on 2/15/18.
+//  Copyright © 2018 Hyper Elephant. All rights reserved.
+//
+
+import XCTest
+@testable import XKCD_Reader
+
+class XKCD_ReaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
